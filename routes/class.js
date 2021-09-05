b4e0bb51b3103ca0ad1330fd87c8478bb33271b4
dawti <dawtiej@gmail.com>
2021-09-05 22:47:07 +0530

    class findall no need of id

--- routes/class.js
+++ routes/class.js
@@ -12,7 +12,7 @@ const router=express.Router();
 router.post("/", classes.create);
 
 // Retrieve all usertype of this user account
-router.get("/:id", classes.findAll);
+router.get("/", classes.findAll);
 
 // Update account details
 router.put("/:id", classes.update);
